10585c1af94ab53d96a461e94d358d940cccc588
Guo Yi Jenny Wong <gyjwong@gmail.com>
2023-08-28 14:23:16 -0700

    eslint, prettier

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // Add a semicolon (;) at the end of every statement
+  singleQuote: true, // Use single quotes (') instead of double quotes (")
+  tabWidth: 2, // One indentation-level tab equals 2 spaces
+  trailingComma: 'all', // Print trailing commas (,) wherever possible
+  useTabs: true, // Indent lines with tabs instead of spaces
+};
